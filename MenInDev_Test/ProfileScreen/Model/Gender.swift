021a980dd01af3e9d4041fb388b273e6a4e02d74
Eugene <evgen29031999@gmail.com>
2022-11-20 15:03:25 +0300

    added Gender enum

---
+++ MenInDev_Test/ProfileScreen/Model/Gender.swift
@@ -0,0 +1,15 @@
+//
+//  Gender.swift
+//  MenInDev_Test
+//
+//  Created by Eugene on 20.11.2022.
+//
+
+import Foundation
+
+enum Gender {
+    case male
+    case female
+    case shmale
+    case ninBinary
+}
